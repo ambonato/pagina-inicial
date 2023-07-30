2effa3ee03827c00aab7fff20540447e2dc85629
Alfredo Bonato <139592413+ambonato@users.noreply.github.com>
2023-07-29 21:41:44 -0300

    Aside/Main/menu
    comportamiento de links de aside
    fondo del main
    comportamiento de links del menu

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-class personaje{
-    constructor(nombre,propietario,inclan){
-        this.nombre= nombre;
-        this.propietario= propietario;
-        this.inclan=inclan;
-        this.informacion=`soy ${this.nombre} mi propietario es ${this.propietario} y ${this.inclan} estoy en el clan`;
+var html5_audiotypes={
+    "mp3": "utl/duck.mp3"}
+    
+    function createsoundbite(sound){
+    var html5audio=document.createElement('audio')
+    if (html5audio.canPlayType){ //Comprobar soporte para audio HTML5
+    for (var i=0; i<arguments.length; i++){
+    var sourceel=document.createElement('source')
+    sourceel.setAttribute('src', arguments[i])
+    if (arguments[i].match(/.(w+)$/i))
+    sourceel.setAttribute('type', html5_audiotypes[RegExp.$1])
+    html5audio.appendChild(sourceel)
     }
-}
-
-let pj =new personaje("Polux","pol" ,"si");
-let pj2 =new personaje("Seizah Moonfate","pol" ,"no");
-
-
-console.log(pj.informacion + " - "+pj2.informacion);
+    html5audio.load()
+    html5audio.playclip=function(){
+    html5audio.pause()
+    html5audio.currentTime=0
+    html5audio.play()
+    }
+    return html5audio
+    }
+    else{
+    return {playclip:function(){throw new Error('Su navegador no soporta audio HTML5')}}
+    }
+    }
+    
+    //Inicializar sonidos
+    var hover1 = createsoundbite('utl/duck.mp3');
+   
